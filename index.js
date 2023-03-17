6b2ed7ee8a8e3d4c3fec532ec2c91e43d2a343b1
lbcanfield <lewie.b.canfield@gmail.com>
2023-03-17 12:26:26 -0500

    Completed function addReview.

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+console.log(reviews[5].feedback)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 (not auto-tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -103,8 +103,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-
-
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews[7].feedback);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function that creates an object with name, rating, feedback, add the new review to the end of an array and returns the resulting array. 
@@ -118,9 +118,16 @@ Use the addReview function below to do the following:
 */
 
 
-function addReview(/*Your Code Here */) {
-     /*Your Code Here */
+function addReview(w, x, y, z) {
+     const newReview = {
+          name: x,
+          rating: y,
+          feedback: z
+     }
+     w.push(newReview)
+     return w;
 }
+
 
 
 
